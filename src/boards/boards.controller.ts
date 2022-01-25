59954ed1bc4005e530c15926dcf2232e1ef53cdc
suekim59 <74885313+suekim59@users.noreply.github.com>
2022-01-25 06:42:17 -0800

    feat:add api for deleteBoard

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -23,5 +23,10 @@ export class BoardsController {
     @Get('/:id')
     getBoardById(@Param('id') id:string):Board {
         return this.boardsService.getBoardById(id);
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id:string):void {
+        this.boardsService.deleteBoard(id);
     }
 }
